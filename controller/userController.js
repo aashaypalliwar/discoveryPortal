d8016ad1c6a384d34bd6d09a9a1669e4ecf94f52
Mintu Jupally <mintu.jupally@gmail.com>
2020-10-26 20:12:38 +0530

    get all reported users done

--- controller/userController.js
+++ controller/userController.js
@@ -12,16 +12,15 @@ function filterObj(obj, ...allowedFields) {
   return newObj;
 }
 
-
 exports.aboutMe = catchAsync(async (req, res, next) => {
   //  Through protect function in auth logic we get the user in req
   console.log(req.user.id);
-  const user = await  User.findById(req.user.id).populate({
+  const user = await User.findById(req.user.id).populate({
     path: 'tags',
     model: 'Tag',
     select: 'name group',
   });
-  
+
   if (!user) {
     return next(new AppError('This user is not present', 401));
   }
@@ -58,15 +57,20 @@ exports.updateProfile = catchAsync(async (req, res, next) => {
   });
 });
 
-
-
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   let filter = {};
-  
+
   let docs;
   // const users = await User.find({role:{$eq:'user'}})
   req.query.sort = 'name';
-  const features = new APIFeatures(User.find(filter), req.query)
+  const features = new APIFeatures(
+    User.find(filter).populate({
+      path: 'tags',
+      model: 'Tag',
+      select: 'name',
+    }),
+    req.query
+  )
     .filter()
     .sort()
     .limitFields()
@@ -83,9 +87,8 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-
-exports.getAllTags = catchAsync(async(req,res,next)=>{
-  let filter={};
+exports.getAllTags = catchAsync(async (req, res, next) => {
+  let filter = {};
   let docs;
   // const users = await User.find({role:{$eq:'user'}})
   req.query.sort = 'group';
@@ -106,33 +109,35 @@ exports.getAllTags = catchAsync(async(req,res,next)=>{
   });
 });
 
-exports.reportUser = catchAsync(async(req,res,next)=>{
-
+exports.reportUser = catchAsync(async (req, res, next) => {
+  console.log('Await 1 starts');
   const reportedUser = await User.findById(req.params.id);
 
-  if(!reportedUser){
-    return next (new AppError('The user with this id is not present',403));
+  if (!reportedUser) {
+    return next(new AppError('The user with this id is not present', 403));
   }
+  console.log('Await 1 done');
 
-  if(reportedUser.reporters &&reportedUser.reporters.includes(req.user.id)){
+  if (reportedUser.reporters && reportedUser.reporters.includes(req.user.id)) {
     res.status(200).json({
-      status : 'success',
-      message : 'This user is already reported by you'
+      status: 'success',
+      message: 'This user is already reported by you',
     });
-  }
-  else{
+  } else {
     // reportedUser.reporters.push(req.user.id);
     // reportedUser.reportCount = reportedUser.reporters.length;
     // await reportedUser.save({runValidators:false});
     const newReportCount = reportedUser.reportCount + 1;
-    const updatedUser = await User.findByIdAndUpdate(req.params.id,{reportCount : newReportCount, $push : {reporters : req.user.id}});
-    res.status({
-      status:'Success',
-      message:'The user has been successfully reported',
-      data : updatedUser
-    })
+    console.log('Await 2 starts');
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      reportCount: newReportCount,
+      $push: { reporters: req.user.id },
+    });
+    console.log('Await 2 done');
+    res.status(200).json({
+      status: 'Success',
+      message: 'The user has been successfully reported',
+      data: updatedUser,
+    });
   }
-
-
-
-})
+});
